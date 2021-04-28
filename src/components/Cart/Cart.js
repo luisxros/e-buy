34a66494b7a0c73a4bd8d6a53c2bc123da4857e2
Luis Rosario <luisalmonter@gmail.com>
2021-04-28 17:07:32 -0400

    adds styles to cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container, Button, Typography, Grid } from '@material-ui/core';
 
+import CartItem from './CartItem/CartItem';
+
 import useStyles from './styles';
 
 const Cart = ({ cart }) => {
@@ -16,7 +18,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
+                        <CartItem item={item}/>
                     </Grid>
                 ))}
             </Grid>
@@ -30,12 +32,12 @@ const Cart = ({ cart }) => {
         </>
     };
 
-    if(!cart.line_items) return 'Loading...';
+    // if(!cart.line_items) return 'Loading...';
 
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant='h3'> Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant='h3' gutterBottom> Your Shopping Cart</Typography>
             { !cart.line_items.length ? <EmptyCart /> : <FilledCart/> }
         </Container>
     )
