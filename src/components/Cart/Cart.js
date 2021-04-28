98cd4f13fab391b7a4e36027f88cd0c348fe7e79
Luis Rosario <luisalmonter@gmail.com>
2021-04-28 19:51:25 -0400

    creates functionality for cart item buttons

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,15 +6,15 @@ import CartItem from './CartItem/CartItem';
 
 import useStyles from './styles';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart,handleUpdateCartQty,handleRemoveFromCart,handleEmptyCart }) => {
 
     const classes = useStyles();
 
-    const EmptyCart = () => {
+    const EmptyCart = () => (
         <Typography variant='subtitle1'> You have no items in your shopping cart!
-            <Link to='/' className={classes.link}>Start adding some</Link>!
+            <Link to='/' className={classes.link}> Start adding some</Link>!
         </Typography>
-    };
+    );
 
     if (!cart.line_items) return 'Loading';
 
@@ -23,14 +23,14 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item}/>
+                        <CartItem item={item}  onRemoveFromCart={handleRemoveFromCart} onUpdateCartQty={handleUpdateCartQty} />
                     </Grid>
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
                 <Typography variant='h4'> Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size='large' type="button" variant="contained" color='secondary'>Empty Cart</Button>
+                    <Button className={classes.emptyButton} size='large' type="button" variant="contained" color='secondary' onClick={handleEmptyCart}>Empty Cart</Button>
                     <Button className={classes.checkoutButton} size='large' type="button" variant="contained" color='primary'>Checkout</Button>
                 </div>
             </div>
